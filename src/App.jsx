cb4b01e263bc26083ca9d1ffa5520c8fce105f30
Alberto <garcia.lara.alberto@gmail.com>
2024-08-15 11:49:51 +0200

    Solved a bug that allowed durations of less than a year

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     duration: 10,
   })
 
+  const inputIsValid = (userInput.duration >= 1)
   function handleChange(inputIdentifier, newValue){
     setUserinput(previusUserInput => {
       return {
@@ -24,7 +25,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput = {userInput} onChange = {handleChange}/>
-      <Results input = {userInput} />
+      {!inputIsValid && (
+        <p className="center">Please, enter a duration greater than 0.</p>
+      )}
+      {inputIsValid && <Results input = {userInput} /> }
     </>
   )
 }
